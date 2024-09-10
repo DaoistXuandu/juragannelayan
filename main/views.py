be6a58ac8a4da3f5c7429bcd3be9e0e0ba86c688
DaoistXuandu <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>
2024-09-10 18:10:53 +0700

    add assignment task

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'app_name': 'Juragan Nelayan',
         'name': 'Raihan Akbar',
         'class': 'PBP E'
     }
